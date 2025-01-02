b2f40af951e43dc28087b968b1553e4688eae6d9
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-02 16:35:22 -0300

    chore: creating and styling header tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { CgArrowsExchangeAltV } from 'react-icons/cg';
+
 export default function HomePage() {
 	return (
 		<>
@@ -9,6 +11,15 @@ export default function HomePage() {
 				}}
 			/>
 			<div className="absolute inset-0 bg-matteBlack bg-opacity-80" />
+
+			<header className="w-full fixed top-[8rem]">
+				<div className="flex items-center justify-center">
+					<CgArrowsExchangeAltV size={36} className="text-orangeRed" />
+					<h1 className="font-alumniSansPinstripe font-semibold bg-gradient-to-r dark:from-orangeRed dark:via-gold dark:to-orangeRed text-transparent dark:bg-clip-text dark:text-[3rem]">
+						Currency Converter
+					</h1>
+				</div>
+			</header>
 		</>
 	);
 }
