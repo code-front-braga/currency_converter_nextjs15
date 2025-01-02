26e63a7c8d101f733ccc8e0c95023c7fe0256724
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-02 16:24:04 -0300

    chore: styling background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 export default function HomePage() {
 	return (
-		<div>
-			<h1>Home Page</h1>
-			<p>Welcome to the home page. This is a static page that is rendered by the server.</p>
-		</div>
+		<>
+			<div
+				className="absolute inset-0 z-0 bg-repeat"
+				style={{
+					backgroundImage: 'url(/glitch-effect.png)',
+					backgroundSize: '120px',
+				}}
+			/>
+			<div className="absolute inset-0 bg-matteBlack bg-opacity-80" />
+		</>
 	);
 }
