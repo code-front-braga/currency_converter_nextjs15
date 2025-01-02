32256c087a2222fdf90d87547d1fc77a2ccdd231
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-02 16:06:21 -0300

    feat: integrate global styles, fonts, and theme handling in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,14 @@
+import clsx from 'clsx';
 import './globals.css';
+
+import { Inter, Alumni_Sans_Pinstripe } from 'next/font/google';
+import { ThemeProvider } from './components/theme-provider';
+
+export const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
+export const alumniSansPinstripe = Alumni_Sans_Pinstripe({
+	subsets: ['latin'],
+	weight: ['400'],
+});
 
 export default function RootLayout({
 	children,
@@ -6,8 +16,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={`antialiased`}>{children}</body>
+		<html lang="pt-br">
+			<body className={clsx('antialiased', inter.className, alumniSansPinstripe.className)}>
+				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
