239d86de19db7c0eb0ac79da68f741fe0889b506
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-02 21:13:48 -0300

    feat: creating, styling and applying functionality in toggle button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx';
 import './globals.css';
 
 import { Inter, Alumni_Sans_Pinstripe } from 'next/font/google';
-import { ThemeProvider } from '../components/theme-provider';
 
 export const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
 export const alumniSansPinstripe = Alumni_Sans_Pinstripe({
@@ -17,11 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="pt-br">
-			<body className={clsx('antialiased', inter.className, alumniSansPinstripe.className)}>
-				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-					{children}
-				</ThemeProvider>
-			</body>
+			<body className={clsx('antialiased', inter.className, alumniSansPinstripe.className)}>{children}</body>
 		</html>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+
+import clsx from 'clsx';
 import { CgArrowsExchangeAltV } from 'react-icons/cg';
+import * as motion from 'motion/react-client';
+import useTheme from '@/components/use-theme';
 
 export default function HomePage() {
+	const { darkMode, setDarkMode } = useTheme();
+
 	return (
 		<>
 			<div
@@ -10,16 +17,45 @@ export default function HomePage() {
 					backgroundSize: '120px',
 				}}
 			/>
-			<div className="absolute inset-0 bg-matteBlack bg-opacity-85" />
+			<div
+				className={clsx('absolute inset-0 bg-opacity-85 transition-colors duration-700', {
+					'bg-snow': !darkMode,
+					'bg-matteBlack': darkMode,
+				})}
+			/>
+			<main className="relative flex h-svh w-full flex-col items-center justify-center p-[1.6rem]">
+				<header className="fixed top-[8rem] w-full">
+					<div className="flex items-center justify-center">
+						<CgArrowsExchangeAltV size={36} className="text-orangeRed" />
+						<h1
+							className={clsx('font-alumniSansPinstripe text-[3rem] font-semibold', {
+								'text-matteBlack': !darkMode,
+								'bg-gradient-to-r text-transparent dark:from-orangeRed dark:via-gold dark:to-orangeRed dark:bg-clip-text':
+									darkMode,
+							})}
+						>
+							Currency Converter
+						</h1>
+					</div>
+				</header>
 
-			<header className="w-full fixed top-[8rem]">
-				<div className="flex items-center justify-center">
-					<CgArrowsExchangeAltV size={36} className="text-orangeRed" />
-					<h1 className="font-alumniSansPinstripe font-semibold bg-gradient-to-r dark:from-orangeRed dark:via-gold dark:to-orangeRed text-transparent dark:bg-clip-text dark:text-[3rem]">
-						Currency Converter
-					</h1>
-				</div>
-			</header>
+				<button
+					className={clsx('relative flex h-[3.5rem] w-[6rem] cursor-pointer rounded-full bg-blue-700 p-[.5rem]', {
+						'justify-start bg-matteBlack': !darkMode,
+						'justify-end bg-black': darkMode,
+					})}
+					onClick={() => setDarkMode(!darkMode)}
+				>
+					<motion.div
+						className={clsx('h-[2.5rem] w-[2.5rem] rounded-full bg-red-600', {
+							'bg-snow': !darkMode,
+							'bg-gradient-to-tr from-orangeRed from-20% via-gold to-matteBlack': darkMode,
+						})}
+						layout
+						transition={{ type: 'spring', visualDuration: 0.5, bounce: 0.4 }}
+					/>
+				</button>
+			</main>
 		</>
 	);
 }
